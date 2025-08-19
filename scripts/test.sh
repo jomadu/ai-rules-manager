7f4dbbb7a91046ce6abf677d759fefbc8c4cab42
jomadu <maxdunn123@gmail.com>
2025-08-19 15:26:33 -0700

    test: update

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,7 @@
 cd ..
 make build
 rm -rf sandbox
+rm -rf ~/.arm/cache
 mkdir sandbox
 cp bin/arm sandbox/
 cd sandbox
@@ -14,8 +15,3 @@ cat .armrc
 tree -a
 ./arm outdated
 tree -a -L 4 ~/.arm/cache
-cat ~/.arm/cache/ruleset-map.json
-cat ~/.arm/cache/registry-map.json
-cat ~/.arm/cache/registries/4d3bb29db1e2e8831cd5a243ad652a476751fa22e7003ab6b80cf62c35053aec/cache-info.json
-cat ~/.arm/cache/registries/4d3bb29db1e2e8831cd5a243ad652a476751fa22e7003ab6b80cf62c35053aec/metadata.json
-cat ~/.arm/cache/registries/4d3bb29db1e2e8831cd5a243ad652a476751fa22e7003ab6b80cf62c35053aec/versions.json
